4281e6314d373508f6d2d7931865200edcdbaeb1
KaliLovesTech <kalilovestech@gmail.com>
2024-01-24 13:41:49 -0500

    complete landing page

--- main.js
+++ main.js
@@ -13,9 +13,36 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }, 1000);
 
-    // Form Submission Handling
-    document.getElementById("signupForm").addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert("Form submitted!");
+    document.getElementById("contactForm").addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        var data = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            travelers: document.getElementById("travelers").value,
+            dateFrom: document.getElementById("dates").value,
+            activities: document.getElementById("activities").value,
+        };
+
+        var formData = new FormData();
+        formData.append("entry.531720767", data.name);
+        formData.append("entry.1472744669", data.email);
+        formData.append("entry.775161060", data.travelers);
+        formData.append("entry.1068319214", data.dates);
+        formData.append("entry.338419322", data.activities);
+
+        fetch("https://docs.google.com/forms/u/0/d/e/1FAIpQLSfmznrlrfNPvcjqn6o_yVLtnbGKv7YH8rfvk9Mnn6AEKgPRww/formResponse", {
+            method: "POST",
+            mode: "no-cors", // Set the mode to 'no-cors'
+            body: formData,
+        })
+            .then(() => {
+                alert("Thank you! Your response has been submitted.");
+                document.getElementById("contactForm").reset();
+                console.error("Form submission failed");
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
     });
 });
